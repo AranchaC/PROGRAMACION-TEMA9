488c2ec327830827ca1cf8657892cdc3864008da
AranchaC <aranchachicharro@gmail.com>
2023-05-04 14:16:30 +0200

    clase 04may

---
+++ Tema9/src/PruebaInicialSql.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+// Añadido en ClassPath /usr/share/java/mariadb-java-client.jar del paquete libmariadb-java en Debian
+// https://stackoverflow.com/questions/23020857/connect-to-mariadb-from-java-application-in-netbeans-on-linux-mageia
+public class PruebaInicialSql {
+	public static void main(String[] args) {
+		try {
+			//Class.forName("org.mariadb.jdbc.Driver");
+			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ropa", "aranchaC",
+					"aranchaDaw@1");
+			Statement statement = connection.createStatement();
+			ResultSet res = statement.executeQuery("select * from persona;");
+			while (res.next()) {
+				System.out.println("ID-"+res.getInt("id")+": NOMBRE-"+ res.getString("nombre") + ": F.NAC-" + res.getString("fnac"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	} // main
+} // class
